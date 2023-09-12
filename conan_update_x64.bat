959b89352c406d0ee1dbae2e43b7108c47ef7b6e
aavasyaev <vasyaevaa@gmail.com>
2023-09-12 16:49:04 +0300

    change resolve script for windows

--- conan_update_x64.bat
+++
@@ -1,8 +0,0 @@
-set current_path=%cd%
-
-
-set conan_dir=%current_path%\.main_cache\conan
-
-
-conan install conanfile.txt -if %conan_dir%\x64\Debug   -s build_type=Debug   -s arch=x86_64 --build=missing
-conan install conanfile.txt -if %conan_dir%\x64\Release -s build_type=Release -s arch=x86_64 --build=missing
---
+++ conan_update_x64.ps1
@@ -0,0 +1,15 @@
+$scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
+
+$conanDir = Join-Path $scriptPath \.main_cache\conan
+
+# Resolve data for Debug mode
+Start-Process -FilePath "conan" -Wait -NoNewWindow -ArgumentList `
+    "install conanfile.txt -if $conanDir\x64\Debug",
+    "-s build_type=Debug",
+    "--build=missing"
+
+# Resolve data for Release mode
+Start-Process -FilePath "conan" -Wait -NoNewWindow -ArgumentList `
+    "install conanfile.txt -if $conanDir\x64\Release",
+    "-s build_type=Release",
+    "--build=missing"
