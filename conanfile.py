509b7eb3c5ef95c35f61e52844116d49d391021e
aavasyaev <vasyaevaa@gmail.com>
2024-04-22 22:23:51 +0300

    feature: update version

--- conanfile.py
+++ conanfile.py
@@ -133,7 +133,7 @@ class CallistoConan(ConanFile):
     dependency_graph : DependencyGraph
 
     name = 'callisto'
-    version = '0.4.0d1'
+    version = '0.4.0d2'
     license = 'BSD'
     package_typ='library'
     settings = 'os', 'compiler', 'build_type', 'arch'
